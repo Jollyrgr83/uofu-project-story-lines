844a444f0572fb01b1c24efeb9780c18b99e3591
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-13 11:50:44 -0600

    Project description fields added

--- public/assets/js/add.js
+++ public/assets/js/add.js
@@ -1,3 +1,38 @@
 $(() => {
   console.log("add");
 });
+
+$(document).ready(() => {
+  // Getting references to our form and inputs
+  const projectForm = $("#project-form");
+  const projectDescription = $("#project-input");
+  // When the form is submitted, we validate there's information
+  projectForm.on("submit", event => {
+    event.preventDefault();
+    const projectData = {
+      description: projectDescription.val().trim()
+    };
+    console.log(projectData);
+    if (!projectData.description) {
+      return;
+    }
+
+    // If we have information present we run the addProject function
+    addProject(projectData.description);
+    projectDescription.val("");
+  });
+
+  // addProject does a post to our "api/add" route and if successful, redirects us the the dashboard page
+  function addProject(description) {
+    $.post("/api/add", {
+      description: description
+    })
+      .then(() => {
+        window.location.replace("/dash");
+        // If there's an error, log the error
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+});
